9c863039c23e01c4da3bc99d13de59998bb8caae
Hexagon <hexagon@56k.guru>
2024-03-30 20:30:37 +0000

    Lower permission requirement of which, by replacing access with stat.

--- src/stat/which.ts
+++ src/stat/which.ts
@@ -1,9 +1,11 @@
-import { access, constants } from "./mod.ts";
+import { constants, stat } from "./mod.ts";
 import { getEnv } from "@cross/env";
 import { join } from "@std/path";
 
 /**
  * Searches for an executable file within the directories specified by the "PATH" environment variable.
+ *
+ * Uses stat to check if anyone (as apposed to the current user) has execute permission on the found executable, to avoid getting the current user id, which require higher permission in certain runtimes.
  *
  * @param command - The name of the executable file to locate.
  * @returns The full path to the executable if found, otherwise null.
@@ -14,8 +16,18 @@ export async function which(command: string): Promise<string | null> {
     for (const dir of paths) {
       const fullPath = join(dir, command);
       try {
-        await access(fullPath, constants.X_OK); // Check if executable
-        return fullPath;
+        const fileInfo = await stat(fullPath);
+        // Soft Permission Check: Check if anyone has execute permission
+        if (
+          fileInfo && fileInfo.mode &&
+          (
+            (fileInfo.mode & constants.S_IXOTH) ||
+            (fileInfo.mode & constants.S_IXUSR) ||
+            (fileInfo.mode & constants.S_IXGRP)
+          )
+        ) {
+          return fullPath;
+        }
       } catch (_err) {
         // Ignore and try the next path
       }
